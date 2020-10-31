fe736cbe64df9c94ba2977f919fe60f33f92db62
itsirovasilis <ioannistsirovasilis@gmail.com>
2020-10-31 15:29:25 +0200

    Refactored project to be AI-first

--- GameController.py
+++ GameController.py
@@ -1,281 +1,268 @@
 import random
-
-import SoundManager
-import GameUI
 import math
 import numpy as np
+import GraphicsManager
+
+I = np.array([[1, 1, 1, 1]])
+J = np.array([[2, 2, 2], [0, 0, 2]])
+L = np.array([[3, 3, 3], [3, 0, 0]])
+O = np.array([[4, 4], [4, 4]])
+S = np.array([[0, 5, 5], [5, 5, 0]])
+T = np.array([[6, 6, 6], [0, 6, 0]])
+Z = np.array([[7, 7, 0], [0, 7, 7]])
+PIECES = [I, J, L, O, S, T, Z]
+PIVOT = [2, 5]
+H_SPEED = 80
+LINES_MULTIPLIERS = [40, 100, 300, 1200]
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 22
+STARTING_LEVEL = 9
+PLAYING_STATE = 1
+GAME_OVER_STATE = 0
+BOARD = np.zeros((BOARD_HEIGHT, BOARD_WIDTH), dtype=int)
+
+
+def calculate_speed(level):
+    return math.floor((0.8 - level * 0.007) ** level * 1000)
+
+
+def get_piece_color(index):
+    return GraphicsManager.PIECES_COLORS[abs(index)]
+
+
+def get_piece_coords():
+    r, c = np.where(BOARD > 0)
+    return np.vstack((r, c))
+
+
+# checking vertical collision for piece's vertical movement
+def v_collision():
+    # c stands for coordinates
+    c = get_piece_coords()
+    for j in range(4):
+        # if piece is going to overlap another piece below, don't move it
+        if BOARD[c[0][j]+1][c[1][j]] < 0:
+            return False
+    return True
+
+
+# checking horizontal collision for piece's horizontal movement
+def h_collision(d):
+    # d stands for direction (-1 for left, +1 for right)
+    # c stands for coordinates
+    c = get_piece_coords()
+    for j in range(4):
+        # if piece is going to move out of the sides, don't move it
+        if c[1][j] + d < 0 or c[1][j] + d > 9:
+            return False
+        # if piece is going to overlap a placed piece, don't move it
+        if BOARD[c[0][j]][c[1][j] + d] < 0:
+            return False
+    # if piece can move, move it
+    return True
+
+
+# horizontal piece movement
+def h_move(d, index):
+    # d is the direction {-1, 1}
+    try:
+        if h_collision(d):
+            c = get_piece_coords()
+            for j in range(4):
+                BOARD[c[0][j]][c[1][j]] = 0
+            # redraw it in its new position, one column right/left
+            for j in range(4):
+                BOARD[c[0][j]][c[1][j] + d] = index
+            # update pivot's position
+            PIVOT[1] += d
+            return True
+    except IndexError:
+        return False
+
+
+# piece rotation (clock-wise)
+# It works with pivoting. When you see the game board the block at first row and sixth column plays the role
+# of the pivot. All other piece's block rotate around this block. That's what this method accomplishes.
+def r_move(ptr, index, r):
+    # ptr stands for piece to rotate
+    # r stands for rotated {True, False}
+
+    # piece O doesn't rotate
+    if np.array_equal(ptr, O):
+        return
+    c = get_piece_coords()
+    # i_rot, j_rot are the coordinates of rotated piece
+    if any(np.array_equal(ptr, p) for p in [I, S, Z]):
+        # I, S and Z have a different rotation. What the code below does is that it returns the piece to its
+        # spawn state every second rotation.
+        # so if it's not r (rotated) then perform a typical rotation
+        if not r:
+            try:
+                for j in range(4):
+                    # if they exceed boundaries, don't rotate
+                    i_rot = -c[1][j] + PIVOT[1]+PIVOT[0]
+                    j_rot = c[0][j] - PIVOT[0] + PIVOT[1]
+                    if i_rot < 0 or i_rot > 21 or j_rot < 0 or j_rot > 9:
+                        return False
+                    # if after rotation, piece overlaps with another piece, don't rotate
+                    if BOARD[i_rot][j_rot] < 0:
+                        return False
+            except IndexError:
+                return False
+            for j in range(4):
+                if c[0][j] != PIVOT[0] or c[1][j] != PIVOT[1]: # pivot block doesn't rotate
+                    # erase block
+                    BOARD[c[0][j]][c[1][j]] = 0
+            for j in range(4):
+                i_rot = -c[1][j] + PIVOT[1] + PIVOT[0]
+                j_rot = c[0][j] - PIVOT[0] + PIVOT[1]
+                if c[0][j] != PIVOT[0] or c[1][j] != PIVOT[1]:
+                    # redraw block in new position
+                    BOARD[i_rot][j_rot] = index
+            return True
+    # else if it is rotated, return it to its original state.
+    # The code below also works for L,J and T pieces.
+    try:
+        for j in range(4):
+            i_rot = c[1][j] - PIVOT[1] + PIVOT[0]
+            j_rot = -c[0][j] + PIVOT[0] + PIVOT[1]
+            if i_rot < 0 or i_rot > 21 or j_rot < 0 or j_rot > 9:
+                return True
+            if BOARD[i_rot][j_rot] < 0:
+                return True
+        for j in range(4):
+            if c[0][j] != PIVOT[0] or c[1][j] != PIVOT[1]:
+                BOARD[c[0][j]][c[1][j]] = 0
+    except IndexError:
+        return True
+    for j in range(4):
+        i_rot = c[1][j] - PIVOT[1] + PIVOT[0]
+        j_rot = -c[0][j] + PIVOT[0] + PIVOT[1]
+        if c[0][j] != PIVOT[0] or c[1][j] != PIVOT[1]:
+            BOARD[i_rot][j_rot] = index
+    return False
+
+
+def spawn_piece(pts):
+    game_over = False
+    reset_pivot()
+    for i in range(pts.shape[0]):
+        for j in range(pts.shape[1]):
+            if pts[i][j] == 0:
+                continue
+            else:
+                if BOARD[(i+2)][(j+4) - (2 - pts.shape[0])] < 0:
+                    game_over = True
+                BOARD[(i+2)][(j+4) - (2 - pts.shape[0])] = pts[i][j]
+    return game_over
+
+
+def reset_pivot():
+    PIVOT[0] = 2
+    PIVOT[1] = 5
+
+
+def game_over():
+    return True
 
 
 class GameController:
     def __init__(self, fps):
-        self.FPS = fps
-        self.I = np.array([[1, 1, 1, 1]])
-        self.J = np.array([[2, 2, 2], [0, 0, 2]])
-        self.L = np.array([[3, 3, 3], [3, 0, 0]])
-        self.O = np.array([[4, 4], [4, 4]])
-        self.S = np.array([[0, 5, 5], [5, 5, 0]])
-        self.T = np.array([[6, 6, 6], [0, 6, 0]])
-        self.Z = np.array([[7, 7, 0], [0, 7, 7]])
-        self.PIECES = [self.I, self.J, self.L, self.O, self.S, self.T, self.Z]
-        self.pivot = [2, 5]
+        self.fps = fps
         self.score = 0
-        self.h_speed = 150
         self.lines = 0
-        self.level = 0
-        self.starting_level = 0
+        self.level = STARTING_LEVEL
+        self.speed = calculate_speed(self.level)
         self.lines_to_level_up = 10
-        self.speed = 1000
-        self.line_multipliers = [40, 100, 300, 1200]
-        self.board = np.array([[0 for j in range(10)] for i in range(22)])
-        self.track = "track1.wav"
-
-        # 0 is Main Menu, 1 is Settings, 2 is Playing, 3 is Game Over
-        self.game_state = 0
-
+        # 1 is Playing, 0 is Game Over
+        self.game_state = 1
         self.rotated = False
         self.drop_counter = 0
         self.move_counter = 0
         self.piece_falling = False
-        self.next_pieces = [np.copy(self.PIECES[random.randint(0, len(self.PIECES) - 1)])]
-        self.theme_playing = False
-        self.game_over_sound_played = False
+        self.next_pieces = [np.copy(PIECES[random.randint(0, len(PIECES) - 1)])]
+        self.can_h_move = True
+        self.can_rotate = True
+
+    def state_initializer(self):
+        self.game_state = PLAYING_STATE
+        BOARD[BOARD != 0] = 0
+        self.level = STARTING_LEVEL
+        self.set_speed(self.level)
+        self.move_counter = 0
+        self.drop_counter = 0
+        self.lines_to_level_up = 10
+        self.lines = 0
+        self.piece_falling = False
+        self.rotated = False
+        self.next_pieces = [np.copy(PIECES[random.randint(0, len(PIECES) - 1)])]
+        self.score = 0
         self.can_h_move = True
         self.can_rotate = True
 
     def set_speed(self, level):
-        self.speed = math.floor((0.8 - level * 0.007) ** level * 1000)
+        self.speed = calculate_speed(level)
 
     def create_piece_sequence(self):
         for i in range(7):
-            self.next_pieces.append(np.copy(self.PIECES[random.randint(0, len(self.PIECES)-1)]))
-
-    def state_initializer(self, game_state, gui: GameUI):
-        self.game_state = game_state
-        gui.screen.fill(gui.BLACK)
-        self.board = np.array([[0 for j in range (10)] for i in range(22)])
-        self.level = self.starting_level
-        self.set_speed(self.starting_level)
-        self.move_counter = 0
-        self.drop_counter = 0
-        self.lines_to_level_up = 10
-        self.lines = 0
-        self.piece_falling = False
-        self.rotated = False
-        self.next_pieces = [np.copy(self.PIECES[random.randint(0, len(self.PIECES) - 1)])]
-        self.theme_playing = False
-        self.game_over_sound_played = False
-        self.score = 0
-        self.can_h_move = True
-        self.can_rotate = True
-
-    def get_piece_color(self, index, gui: GameUI):
-        if index == 0:
-            return gui.GREY
-        elif abs(index) == 1:
-            return gui.CYAN
-        elif abs(index) == 2:
-            return gui.BLUE
-        elif abs(index) == 3:
-            return gui.ORANGE
-        elif abs(index) == 4:
-            return gui.YELLOW
-        elif abs(index) == 5:
-            return gui.GREEN
-        elif abs(index) == 6:
-            return gui.PURPLE
-        else:
-            return gui.RED
-
-    def get_piece_coords(self):
-        r, c = np.where(self.board > 0)
-        return np.vstack((r, c))
-
-    # checking vertical collision for piece's vertical movement
-    def v_collision(self):
-        # c stands for coordinates
-        c = self.get_piece_coords()
-        for j in range(4):
-            # if piece is going to overlap another piece below, don't move it
-            if self.board[c[0][j]+1][c[1][j]] < 0:
-                return False
-        return True
-
-    # checking horizontal collision for piece's horizontal movement
-    def h_collision(self, d):
-        # d stands for direction (-1 for left, +1 for right)
-        # c stands for coordinates
-        c = self.get_piece_coords()
-        for j in range(4):
-            # if piece is going to move out of the sides, don't move it
-            if c[1][j] + d < 0 or c[1][j] + d > 9:
-                return False
-            # if piece is going to overlap a placed piece, don't move it
-            if self.board[c[0][j]][c[1][j] + d] < 0:
-                return False
-        # if piece can move, move it
-        return True
+            self.next_pieces.append(np.copy(PIECES[random.randint(0, len(PIECES)-1)]))
 
     # vertical piece movement
-    def v_move(self, index, sm: SoundManager):
+    def v_move(self, index):
         # if piece tries to move out of bounds, except IndexError
         try:
             # if there space to move vertically, then move
-            if self.v_collision():
+            if v_collision():
                 # c stands for coordinates
-                c = self.get_piece_coords()
+                c = get_piece_coords()
                 # erase piece from board
                 for j in range(4):
-                    self.board[c[0][j]][c[1][j]] = 0
+                    BOARD[c[0][j]][c[1][j]] = 0
                 # redraw it in its new position, one row lower
                 for j in range(4):
-                    self.board[c[0][j]+1][c[1][j]] = index
+                    BOARD[c[0][j]+1][c[1][j]] = index
                 # update pivot's position
-                self.pivot[0] += 1
+                PIVOT[0] += 1
                 return True
-            else:
-                self.update_board(index, sm)
-                return False
+            raise IndexError()
         except IndexError:
-            self.update_board(index, sm)
+            self.update_board(index)
             return False
 
-    # horizontal piece movement
-    def h_move(self, d, index):
-        # d is the direction {-1, 1}
-        try:
-            if self.h_collision(d):
-                c = self.get_piece_coords()
-                for j in range(4):
-                    self.board[c[0][j]][c[1][j]] = 0
-                # redraw it in its new position, one column right/left
-                for j in range(4):
-                    self.board[c[0][j]][c[1][j] + d] = index
-                # update pivot's position
-                self.pivot[1] += d
-                return True
-        except IndexError:
-            return False
-
-    # piece rotation (clock-wise)
-    # It works with pivoting. When you see the game board the block at first row and sixth column plays the role
-    # of the pivot. All other piece's block rotate around this block. That's what this method accomplishes.
-    def r_move(self, ptr, index, r):
-        # p stands for piece to rotate
-        # r stands for rotated {True, False}
-
-        # piece O doesn't rotate
-        if np.array_equal(ptr, self.O):
-            return
-        c = self.get_piece_coords()
-        # i_rot, j_rot are the coordinates of rotated piece
-        if any(np.array_equal(ptr, p) for p in [self.I, self.S, self.Z]):
-            # I, S and Z have a different rotation. What the code below does is that it returns the piece to its
-            # spawn state every second rotation.
-            # so if it's not r (rotated) then perform a typical rotation
-            if not r:
-                try:
-                    for j in range(4):
-                        # if they exceed boundaries, don't rotate
-                        i_rot = -c[1][j]+self.pivot[1]+self.pivot[0]
-                        j_rot = c[0][j] - self.pivot[0] + self.pivot[1]
-                        if i_rot < 0 or i_rot > 21 or j_rot < 0 or j_rot > 9:
-                            return False
-                        # if after rotation, piece overlaps with another piece, don't rotate
-                        if self.board[i_rot][j_rot] < 0:
-                            return False
-                except IndexError:
-                    return False
-                for j in range(4):
-                    if c[0][j] != self.pivot[0] or c[1][j] != self.pivot[1]: # pivot block doesn't rotate
-                        # erase block
-                        self.board[c[0][j]][c[1][j]] = 0
-                for j in range(4):
-                    i_rot = -c[1][j] + self.pivot[1] + self.pivot[0]
-                    j_rot = c[0][j] - self.pivot[0] + self.pivot[1]
-                    if c[0][j] != self.pivot[0] or c[1][j] != self.pivot[1]:
-                        # redraw block in new position
-                        self.board[i_rot][j_rot] = index
-                return True
-        # else if it is rotated, return it to its original state.
-        # The code below also works for L,J and T pieces.
-        try:
-            for j in range(4):
-                i_rot = c[1][j] - self.pivot[1] + self.pivot[0]
-                j_rot = -c[0][j] + self.pivot[0] + self.pivot[1]
-                if i_rot < 0 or i_rot > 21 or j_rot < 0 or j_rot > 9:
-                    return True
-                if self.board[i_rot][j_rot] < 0:
-                    return True
-            for j in range(4):
-                if c[0][j] != self.pivot[0] or c[1][j] != self.pivot[1]:
-                    self.board[c[0][j]][c[1][j]] = 0
-        except IndexError:
-            return True
+    def update_board(self, index):
+        c = get_piece_coords()
         for j in range(4):
-            i_rot = c[1][j] - self.pivot[1] + self.pivot[0]
-            j_rot = -c[0][j] + self.pivot[0] + self.pivot[1]
-            if c[0][j] != self.pivot[0] or c[1][j] != self.pivot[1]:
-                self.board[i_rot][j_rot] = index
-        return False
-
-    def update_board(self, index, sm: SoundManager):
-        c = self.get_piece_coords()
-        for j in range(4):
-            self.board[c[0][j]][c[1][j]] = -index
+            BOARD[c[0][j]][c[1][j]] = -index
         # returns true if every element in a row is non-zero, else false, for each row
-        lines_state = np.all(self.board, axis=1)
+        lines_state = np.all(BOARD, axis=1)
         lines_to_erase = 0
         for i in range(lines_state.shape[0]):
             # if line is full of piece blocks
             if lines_state[i]:
                 lines_to_erase += 1
-                self.board[i] = 0
+                BOARD[i] = 0
                 for r in reversed(range(i+1)):
                     if r > 0:
-                        self.board[r] = np.copy(self.board[r-1])
-        if lines_to_erase == 4:
-            sm.play_sound("tetris.wav", 2, 0)
-        elif lines_to_erase > 0:
-            sm.play_sound("line-remove.wav", 2, 0)
-        else:
-            sm.play_sound("piece-fall.wav", 2, 0)
+                        BOARD[r] = np.copy(BOARD[r-1])
         self.lines += lines_to_erase
         if lines_to_erase > 0:
-            temp = (self.level+1)*self.line_multipliers[lines_to_erase-1]
+            temp = (self.level+1)*LINES_MULTIPLIERS[lines_to_erase-1]
             if self.score + temp > 999999:
                 self.score = 999999
             else:
                 self.score += temp
-        # The game levels up every 10 line clearances. There is catch though.
+        # The game levels up every 10 line clearances. There is a catch though.
         # For the game to reach level 10, the player has to clear 100 lines.
         # So if you start from level 9, the game won't level up at the first 10 lines but when you clear 100 lines.
         if self.level != 9:
             if self.lines >= self.lines_to_level_up:
                 self.lines_to_level_up += 10
-                self.level_up(sm)
+                self.level_up()
         elif self.level == 9:
             if self.lines >= 100:
                 self.lines_to_level_up = 110
-                self.level_up(sm)
+                self.level_up()
 
-    def level_up(self, sm: SoundManager):
+    def level_up(self):
         self.level += 1
         self.set_speed(self.level)
-        sm.play_sound("level-up.wav", 4, 0)
-
-    def spawn_piece(self, pts):
-        game_over = False
-        self.pivot = [2, 5]
-        for i in range(pts.shape[0]):
-            for j in range(pts.shape[1]):
-                if pts[i][j] == 0:
-                    continue
-                else:
-                    if self.board[(i+2)][(j+4) - (2 - pts.shape[0])] < 0:
-                        game_over = True
-                    self.board[(i+2)][(j+4) - (2 - pts.shape[0])] = pts[i][j]
-        return game_over
-
-    def game_over(self, sm: SoundManager):
-        sm.play_sound("game-over.wav", 0, 0)
-        return True
--- Tetris.py
+++ Tetris.py
@@ -1,82 +1,32 @@
 import pygame as pg
 import numpy as np
 import sys
-from SoundManager import SoundManager
-from MainMenuUI import MainMenuUI
-from SettingsUI import SettingsUI
-from GameUI import GameUI
-from GameController import GameController
+import GraphicsManager
+import GameController
+
+WIDTH = 1186
+HEIGHT = 964
+FPS = 60
+INSTANT_DROP_SPEED = 1000
 
 
 def main():
-    WIDTH = 1186
-    HEIGHT = 964
-    sm = SoundManager("sounds/")
     pg.init()
     pg.font.init()
-    mui = MainMenuUI(WIDTH, HEIGHT)
-    sui = SettingsUI(WIDTH, HEIGHT)
-    gui = GameUI(WIDTH, HEIGHT)
-    gc = GameController(60)
+    gui = GraphicsManager.GraphicsManager(WIDTH, HEIGHT)
+    gc = GameController.GameController(FPS)
     clock = pg.time.Clock()
     while True:
         # The time it takes for a frame to complete. It is used to make the game frame independent.
-        dt = clock.tick(gc.FPS)
+        dt = clock.tick(gc.fps)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sys.exit()
-        if gc.game_state == 0:
-            if gc.starting_level != 0:
-                gc.starting_level = 0
-                gc.set_speed(gc.starting_level)
-            if pg.mouse.get_pressed()[0]:
-                mouse = pg.mouse.get_pos()
-                # If player clicks on play button then go to settings
-                if WIDTH // 2 - 90 < mouse[0] < WIDTH // 2 + 90 and HEIGHT // 2 - 115 < mouse[1] < HEIGHT // 2 - 35:
-                    gc.game_state = 1
-                # If player clicks on quit button, exit
-                if WIDTH // 2 - 90 < mouse[0] < WIDTH // 2 + 90 and HEIGHT // 2 + 35 < mouse[1] < HEIGHT // 2 + 115:
-                    sys.exit()
-            # At game over the music stops. This ensures that the music will start again.
-            if not gc.theme_playing:
-                gc.theme_playing = True
-                gc.track = "track1.wav"
-                sm.play_sound(gc.track, 0, -1)
-            mui.draw_main_menu()
-        elif gc.game_state == 1:
-            if pg.mouse.get_pressed()[0]:
-                mouse = pg.mouse.get_pos()
-                # Start button
-                if WIDTH // 2 - 90 < mouse[0] < WIDTH // 2 + 90 and HEIGHT // 2 + 300 < mouse[1] < HEIGHT // 2 + 380:
-                    gc.game_state = 2
-                    gc.level = gc.starting_level
-                # Track buttons
-                if WIDTH // 2 - 275 < mouse[0] < WIDTH // 2 - 125:
-                    for i in range(3):
-                        dy = (i+1)*70
-                        if HEIGHT // 2 - 148 + dy < mouse[1] < HEIGHT // 2 - 148 + 50 + dy:
-                            gc.track = "track" + str(i+1) + ".wav"
-                            sm.play_sound(gc.track, 0, -1)
-                # Even levels buttons
-                if WIDTH // 2 + 240 < mouse[0] < WIDTH // 2 + 290:
-                    for i in range(5):
-                        dy = 50*(i+1)
-                        if HEIGHT // 2 - 140 + dy < mouse[1] < HEIGHT // 2 - 140 + 50 + dy:
-                            gc.starting_level = 2*i
-                            gc.set_speed(gc.starting_level)
-                # Even levels buttons
-                if WIDTH // 2 + 290 < mouse[0] < WIDTH // 2 + 340:
-                    for i in range(5):
-                        dy = 50*(i+1)
-                        if HEIGHT // 2 - 140 + dy < mouse[1] < HEIGHT // 2 - 140 + 50 + dy:
-                            gc.starting_level = 2*i+1
-                            gc.set_speed(gc.starting_level)
-            sui.draw_settings(gc.starting_level)
-        elif gc.game_state == 2:
+        if gc.game_state == GameController.PLAYING_STATE:
             gc.drop_counter += dt
             gc.move_counter += dt
             # Delay between player's side movement
-            if gc.move_counter >= gc.h_speed:
+            if gc.move_counter >= GameController.H_SPEED:
                 gc.can_h_move = True
             if not pg.key.get_pressed()[pg.K_UP]:
                 gc.can_rotate = True
@@ -89,41 +39,32 @@ def main():
                 del gc.next_pieces[0]
                 next_piece = gc.next_pieces[:][0]
                 index = pts[pts > 0][0]
-                game_over = gc.spawn_piece(pts)
+                game_over = GameController.spawn_piece(pts)
                 if game_over:
-                    gc.game_state = 3
+                    gc.game_state = GameController.GAME_OVER_STATE
                 pf = pts[:]
             else:
                 if pg.key.get_pressed()[pg.K_DOWN]:
-                    gc.drop_counter *= 3
-                if pg.key.get_pressed()[pg.K_LEFT] and gc.move_counter >= gc.h_speed and gc.can_h_move:
+                    # THIS SHOULD BE CHANGED
+                    gc.drop_counter = INSTANT_DROP_SPEED
+                if pg.key.get_pressed()[pg.K_LEFT] and gc.move_counter >= GameController.H_SPEED and gc.can_h_move:
                     gc.can_h_move = False
                     gc.move_counter = 0
-                    if gc.h_move(-1, index):
-                        sm.play_sound("piece-move.wav", 1, 0)
-                if pg.key.get_pressed()[pg.K_RIGHT] and gc.move_counter >= gc.h_speed and gc.can_h_move:
+                    GameController.h_move(-1, index)
+                if pg.key.get_pressed()[pg.K_RIGHT] and gc.move_counter >= GameController.H_SPEED and gc.can_h_move:
                     gc.can_h_move = False
                     gc.move_counter = 0
-                    if gc.h_move(1, index):
-                        sm.play_sound("piece-move.wav", 1, 0)
+                    GameController.h_move(1, index)
                 if pg.key.get_pressed()[pg.K_UP] and gc.can_rotate:
                     gc.can_rotate = False
-                    sm.play_sound("piece-rotate.wav", 3, 0)
-                    gc.rotated = gc.r_move(pf, index, gc.rotated)
+                    gc.rotated = GameController.r_move(pf, index, gc.rotated)
                 if gc.drop_counter >= gc.speed and gc.piece_falling:
                     gc.drop_counter = 0
-                    gc.piece_falling = gc.v_move(index, sm)
-            gui.screen.fill(gui.BLACK)
-            gui.draw_game_ui(gc, next_piece, np.array_equal(next_piece, gc.I))
-        elif gc.game_state == 3:
-            if not gc.game_over_sound_played:
-                gc.game_over_sound_played = gc.game_over(sm)
-            if pg.key.get_pressed()[pg.K_SPACE]:
-                gc.state_initializer(2, gui)
-                sm.play_sound(gc.track, 0, -1)
-            if pg.key.get_pressed()[pg.K_ESCAPE]:
-                gc.state_initializer(0, gui)
-                sm.play_sound(gc.track, 0, -1)
+                    gc.piece_falling = gc.v_move(index)
+            gui.screen.fill(GraphicsManager.BLACK)
+            gui.draw_game_ui(gc, next_piece, np.array_equal(next_piece, GameController.I))
+        else:
+            gc.state_initializer()
         pg.display.update()
 
 
